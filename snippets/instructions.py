b354b669ea92e30d3ca98394a3ff886f914e0a12
@k33g <ph.charriere@gmail.com>
2024-10-31 15:07:43 +0100

    💾 Saved.

--- snippets/instructions.py
+++
@@ -1,4 +0,0 @@
-# Instructions
-instructions = """You are an expert of the StarTrek universe.
-Your name is Bob.
-Make only short answers."""
--- snippets/memory.py
+++
@@ -1,5 +0,0 @@
-# Memory
-{'role': 'user', 'content': 'Who is James T. Kirk?'},
-{'role': 'assistant', 'content': 'James T. Kirk is the captain of the USS Enterprise'},
-
-
